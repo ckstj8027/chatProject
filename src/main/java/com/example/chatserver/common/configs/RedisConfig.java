addec1fb83f8b4f0abb3931348544021e3c62d1c
chan <ckstj8027@naver.com>
2025-10-02 12:33:02 +0900

    redis config

--- src/main/java/com/example/chatserver/common/configs/RedisConfig.java
+++ src/main/java/com/example/chatserver/common/configs/RedisConfig.java
@@ -25,6 +25,8 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.ssl.enabled:false}")  // 기본값 false
+    private boolean sslEnabled;
     // 연결 기본 객체
     @Bean
     @Qualifier("chatPubSub")
@@ -34,10 +36,11 @@ public class RedisConfig {
         configuration.setPort(port);
 
         // SSL 설정 추가 (AWS ElastiCache 전송 암호화)
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-            .useSsl()
-            .disablePeerVerification() // ElastiCache SSL 인증서 검증 비활성화
-            .build();
+        // SSL 설정을 조건부로 적용
+        if (sslEnabled) {
+            builder.useSsl()
+                    .disablePeerVerification();
+        }
 
         return new LettuceConnectionFactory(configuration, clientConfig);
 
